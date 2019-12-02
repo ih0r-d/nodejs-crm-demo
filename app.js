34d730c8f591c2be9021fb299894892b440106e2
Ihor Didyk <ihordidyk95@gmail.com>
2019-12-02 14:03:26 +0200

    configured connect to mongodb cluster

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require('express')
 
+const mongoose = require('mongoose')
 const parser = require('body-parser')
+
 const authRoutes = require('./routes/auth')
 const analyticsRoutes = require('./routes/analytics')
 const categoryRoutes = require('./routes/category')
 const orderRoutes = require('./routes/order')
 const positionRoutes = require('./routes/position')
 
+const props = require('./config/properties')
+
 const app = express()
 
+mongoose.connect(props.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+    }).then(() => console.log('Connected to MongoDb successfully'
+    )).catch(e => console.log(e))
 
 app.use(require('morgan')('dev'))
 app.use(parser.urlencoded({extended: true}))
---
+++ config/properties.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MONGODB_URI: 'mongodb://ulggl9eoqxlvbfemdfbb:BlbQbf5KUKKni9z5OjQH@blk6pbfo0ipzevp-mongodb.services.clever-cloud.com:27017/blk6pbfo0ipzevp'
+}
